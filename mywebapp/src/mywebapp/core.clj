41f763aeb2daecdb59564b11864398d8d7a95743
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-10-03 07:27:24 -0300

    simple web app in clojure

--- mywebapp/src/mywebapp/core.clj
+++ mywebapp/src/mywebapp/core.clj
@@ -1,6 +1,23 @@
-(ns mywebapp.core)
+(ns mywebapp.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :refer [wrap-params]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn myapp [request]
+  (str "Hello, " (get (:params request) "name")))
+
+(defn string-response-middleware [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (if (instance? String response)
+        {:body response
+         :status 200
+         :headers {"Content-Type" "text/html"}}
+        response))))
+
+(def handler
+  (-> myapp
+      string-response-middleware
+      wrap-params))
+
+(defn -main []
+  (jetty/run-jetty handler {:port 3000}))
