dfbfddcf01b381808fe344b8d00c297442395824
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-10-03 07:54:21 -0300

    Route using compojure

--- mywebapp/src/mywebapp/core.clj
+++ mywebapp/src/mywebapp/core.clj
@@ -1,10 +1,22 @@
 (ns mywebapp.core
-  (:require [compojure.core :refer [defroutes GET POST]]
+  (:require [compojure.core :refer [context defroutes GET POST]]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.params :refer [wrap-params]]))
 
 (defroutes myapproutes
-  (GET "/" [] "Hello from Compojure"))
+  (context "/admin" []
+      (GET "/login" [] (str "Logging in id" id))
+      (GET "/logout" [] "Logging out"))
+  (GET "/" [] "Hello from Compojure")
+  (POST "/:name" [name] (fn [req] (str "Hello, "(-> req :route-params :name))))
+  (GET "/hex-id/:id" :id #"[a-fA-F0-9]+"] [id] (str "ID: " id))
+  (GET "/route1" [] "Hello from route1")
+  (GET "/route2" [] "Hello form route2")
+  (route/resources "/static"))
+
+(myapproutes {:uri "/" :request-method :get})
+(myapproutes {:uri "/admin/1/login" :request-method :get})
+
 
 (defn myapp [request]
   (str "Hello, " (get (:params request) "name")))
