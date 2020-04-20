dc752d49058cb0a2487b28c3a33ded6181c9f5dd
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-04-19 22:12:37 -0300

    Add some functions to use routes get and post to add todos and get todos and todo

--- codes-examples/api-example/project.clj
+++ codes-examples/api-example/project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns api-example.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring/ring-core "1.7.1"]
+                 [metosin/reitit-ring "0.4.2"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-json "0.4.0"]]
+  :main ^:skip-aot api-example.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
