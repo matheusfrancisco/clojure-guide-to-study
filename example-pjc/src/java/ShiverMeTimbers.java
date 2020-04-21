5be099744fa39d26cdbceedc71d79f21b2746845
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-04-21 18:34:13 -0300

    Create project clojure to bind lib java and code java

---
+++ example-pjc/src/java/ShiverMeTimbers.java
@@ -0,0 +1,11 @@
+public class ShiverMeTimbers {
+    public static void main()
+    {
+        System.out.println("Shiver me timbers!!!");
+    }
+
+    public static void thisMain()
+    {
+        System.out.println("This Main!!!");
+    }
+}
